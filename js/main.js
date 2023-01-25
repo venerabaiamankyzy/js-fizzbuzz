bba0c545b1d9981423a07270deb856578fd8de8b
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-01-25 16:23:11 +0100

    + li & classes

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const listEl = document.getElementById("my-list");
+
+
+for (let i = 1; i <= 100; i++) {
+    const listItemEl = document.createElement('li');
+    listItemEl.classList.add('box');
+    listEl.append(listItemEl);
+
+  if (i % 15 == 0) {
+    listItemEl.classList.add('fifteenth-box');
+  } else if (i % 5 == 0) {
+    listItemEl.classList.add('fifth-box');
+  } else if (i % 3 == 0) {
+    listItemEl.classList.add('third-box');
+  } 
+     
+}
+      
+
+// Consegna:
+
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi in console "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
+// Per i numeri che sono sia multipli di 3 che di 5 stampi in console "FizzBuzz".
+
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+
+// Come faccio a sapere se un numero è divisibile per un altro?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+
+// Consigli del giorno:
+
+// 1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+// BONUS 1:
+// Crea un container nel DOM , aggiungendo (attraverso la funzione `append()`) un elemento html con il numero o la stringa corretta da mostrare.
+
+// BONUS 2:
+// Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+// Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
